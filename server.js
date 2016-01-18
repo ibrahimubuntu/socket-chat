e5a97c81752ed6f85740200d4df3f07ea4459854
Ibrahim Ubuntu <ibrahim_mazen@outlook.com>
2016-01-19 00:35:58 +0200

    add disconnect event

--- server.js
+++ server.js
@@ -18,12 +18,28 @@ var clientInfo = {
 io.on('connection',function (socket){
 	console.log('user connected via io');
 	
+	socket.on('disconnect',function (){
+		var userData = clientInfo[socket.id];
+		if (typeof userData !== 'undefined')
+		{
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message',{
+				name: 'System',
+				text: userData.name + ' has left the room !!',
+				timeStamp: moment().valueOf()
+			});
+
+		delete clientInfo[socket.id];	
+		}
+	});
+
+
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
 		socket.broadcast.to(req.room).emit('message',{
 			name: 'System',
-			text: req.name+ ' has joined !!',
+			text: req.name + ' has joined !!',
 			timeStamp: moment().valueOf()
 		});
 	});
