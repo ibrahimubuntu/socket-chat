73d0768eedc0fad9fbb01668f03e9d2e4a35508b
Ibrahim Ubuntu <ibrahim_mazen@outlook.com>
2016-01-19 01:05:31 +0200

    done backend

--- server.js
+++ server.js
@@ -1,65 +1,85 @@
-var PORT= process.env.PORT || 3000;
-var express = require('express');
-
+var PORT = process.env.PORT || 3000;
 var moment = require('moment');
-var now = moment();
-
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io =require('socket.io')(http);
-
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-var clientInfo = {
+var clientInfo = {};
 
-};
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
 
-io.on('connection',function (socket){
-	console.log('user connected via io');
-	
-	socket.on('disconnect',function (){
-		var userData = clientInfo[socket.id];
-		if (typeof userData !== 'undefined')
-		{
-			socket.leave(userData.room);
-			io.to(userData.room).emit('message',{
-				name: 'System',
-				text: userData.name + ' has left the room !!',
-				timeStamp: moment().valueOf()
-			});
+	if (typeof info === 'undefined') {
+		return;
+	}
 
-		delete clientInfo[socket.id];	
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
 		}
 	});
 
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
+io.on('connection', function (socket) {
+	console.log('User connected via socket.io!');
+
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
 
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
-		socket.broadcast.to(req.room).emit('message',{
+		socket.broadcast.to(req.room).emit('message', {
 			name: 'System',
-			text: req.name + ' has joined !!',
-			timeStamp: moment().valueOf()
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
 		});
 	});
 
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
 
-	socket.on('message',function (message){
-		message.timeStamp = moment().valueOf();
-		console.log('Message recieved: '+ message.text);
-		io.to(clientInfo[socket.id].room).emit('message',message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);	
+		}
 	});
 
-	socket.emit('message',{
+	// timestamp property - JavaScript timestamp (milliseconds)
+
+	socket.emit('message', {
 		name: 'System',
-		timeStamp:moment().valueOf(),
-		text: 'Welcome to EE-chat application'
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
-
 });
 
-http.listen(PORT,function(){
-	console.log ('Server is up');
+http.listen(PORT, function () {
+	console.log('Server started!');
 });
-
