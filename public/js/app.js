48e18ac01ef6e8dc040b9bd4bca723b926668908
Ibrahim Ubuntu <ibrahim_mazen@outlook.com>
2016-01-19 00:10:52 +0200

    add rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,14 @@ var name = getQueryVariable('name') || 'Annonymous';
 var room = getQueryVariable('room');
 var socket = io();
 
+jQuery('.room-title').text(room);
+
 socket.on('connect',function(){
 	console.log('Successfully connect to server');
+	socket.emit('joinRoom',{
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message',function (message){
--- server.js
+++ server.js
@@ -10,13 +10,29 @@ var io =require('socket.io')(http);
 
 
 app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {
+
+};
+
 io.on('connection',function (socket){
 	console.log('user connected via io');
 	
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message',{
+			name: 'System',
+			text: req.name+ ' has joined !!',
+			timeStamp: moment().valueOf()
+		});
+	});
+
+
 	socket.on('message',function (message){
 		message.timeStamp = moment().valueOf();
 		console.log('Message recieved: '+ message.text);
-		io.emit('message',message);
+		io.to(clientInfo[socket.id].room).emit('message',message);
 	});
 
 	socket.emit('message',{
